5b81aa3cb2b563f7aea09e7553332599fc1b01fd
yilmazme <10myilmaz@gmail.com>
2022-03-02 15:24:35 +0300

    favicon addded

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,72 +1,111 @@
 const myForm = document.getElementById("myForm");
 const player = document.getElementById("player");
 
-myForm.addEventListener("submit", async (e) => {
+
+
+myForm.addEventListener("submit", (e) => {
+
   e.preventDefault();
+  const data = {
+     text1 : document.querySelector("#text1").value.toUpperCase(),
+     text2 : document.querySelector("#text2").value.toUpperCase(),
+     text3 : document.querySelector("#text3").value.toUpperCase(),
+     text4 : document.querySelector("#text4").value.toUpperCase(),
+     text5 : document.querySelector("#text5").value.toUpperCase(),
+     text6 : document.querySelector("#text6").value.toUpperCase(),
+  };
 
-  let formData = new FormData(myForm)
-
-  for(let pair of formData) {
-    console.log(`${pair[0]} : ${pair[1]}`);
-  }
-//   e.preventDefault();
-
-//   let response = await fetch(url, {
-//     method: 'POST',
-//     body: formData
-//   });
-
-//   let result = await response.json();
-
+  fetch("https://api-thor.leadport.app/api/Sip/changeVideo", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then(data => {
+        changeVideo(JSON.stringify(data))
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
 
+// for onload
 
-window.addEventListener("dblclick", async (e)=>{
+window.addEventListener("load", ()=>{
+    const data = {
+        text1 : document.querySelector("#text1").value,
+        text2 : document.querySelector("#text2").value,
+        text3 : document.querySelector("#text3").value,
+        text4 : document.querySelector("#text4").value,
+        text5 : document.querySelector("#text5").value,
+        text6 : document.querySelector("#text6").value,
+      };
     
-    // e.preventDefault();
-    let lottiePlayer = document.getElementById("lottie-player");
-    lottiePlayer.remove();
-    let newPlayer = document.createElement("lottie-player")
-    
-    attributeArray.forEach(element => {
-        newPlayer.setAttribute(element.name, element.value)
-    });
+      fetch("https://api-thor.leadport.app/api/Sip/changeVideo", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then(data => {
+            changeVideo(JSON.stringify(data))
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
 
-    player.appendChild(newPlayer);
 })
 
+function changeVideo(url){
 
-let attributeArray = [
-    {
-        name: "id",
-        value: "lottie-player"
-    },
-    {
-        name: "src",
-        value: "assets/data/data.json"
-    },
-    {
-        name: "background",
-        value: "transparent"
-    },
-    {
-        name: "speed",
-        value: 1
-    },
-    {
-        name: "style",
-        value: "width: 400px; height: 400px; margin: auto; padding: 1rem;"
-    },
-    {
-        name: "loop",
-        value: ""
-    },
-    {
-        name: "controls",
-        value: ""
-    },
-    {
-        name: "autoplay",
-        value: ""
-    },
-]
+    let attributeArray = [
+        {
+          name: "id",
+          value: "lottie-player",
+        },
+        {
+          name: "src",
+          value: url,
+        },
+        {
+          name: "background",
+          value: "transparent",
+        },
+        {
+          name: "speed",
+          value: 1,
+        },
+        {
+          name: "style",
+          value: "width: 600px; height: 800px; margin: auto; ",
+        },
+        {
+          name: "loop",
+          value: "",
+        },
+        {
+          name: "controls",
+          value: "",
+        },
+        {
+          name: "autoplay",
+          value: "",
+        },
+      ];
+
+    let lottiePlayer = document.getElementById("lottie-player");
+    lottiePlayer.remove();
+    let newPlayer = document.createElement("lottie-player");
+  
+    attributeArray.forEach((element) => {
+      newPlayer.setAttribute(element.name, element.value);
+    });
+  
+    player.appendChild(newPlayer);
+}
+
+
